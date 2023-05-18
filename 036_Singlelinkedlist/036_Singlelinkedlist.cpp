8ecd441d93f2cc2b357a6146b3bd83347533cfa7
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-18 20:54:43 +0700

    listEmpty digunakan untuk memeriksa apakah linked list kosong atau tidak.

--- 036_Singlelinkedlist/036_Singlelinkedlist.cpp
+++ 036_Singlelinkedlist/036_Singlelinkedlist.cpp
@@ -75,3 +75,10 @@ bool deleteNode(int nim) {
         START = current->next;
     return true;
 }
+
+bool listEmpty() {
+    if (START == NULL)
+        return true;
+    else
+        return false;
+}
